f0d923f697580c631ea59661a84388bbe00eda2b
Rushil Mahadevu <103291736+RushilMahadevu@users.noreply.github.com>
2024-11-20 09:12:07 -0800

    Update main.py

--- main.py
+++ main.py
@@ -16,13 +16,13 @@ while True:
     firstsecond = time.time()
     print(display)
     if display.startswith("y") or display.startswith("s"):
-        print("\nSeconds    Times    Subnum     Num")
+        print("\nSeconds | Times | Subnum | Num")
     if num == 0:
         print("\n The number is 0")
     else:
         while num > 0:
             finaltime = time.time()
-            totaltime = round(finaltime - firstsecond, 3)
+            totaltime = round(finaltime - firstsecond, 1)
             num -= subnum
             subnum += 2
             times += 1
@@ -30,11 +30,11 @@ while True:
                 print(f"{totaltime}         {times}         {subnum}        {num}")
         if num == 0:
             if display.startswith("y") or display.startswith("s"):
-                print("\nSeconds    Times    Subnum     Num")
+                print("\nSeconds | Times | Subnum | Num")
             print(f"\nThe square root of the number is: {times}")
         elif num < 0:
             if display.startswith("y") or display.startswith("s"):
-                print("\nSeconds       Times       Subnum        Num")
+                print("\nSeconds | Times | Subnum | Num")
             print("\nThe number is not a perfect square.")
     repeat = input("\nDo you want to calculate another square root? (y/n): ")
     if repeat.startswith("n"):
